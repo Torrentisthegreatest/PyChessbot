b09419c7da4c0fa25230ea525d807d1e9c4a0c2d
Torrentisthegreatest <24814086+Torrentisthegreatest@users.noreply.github.com>
2019-04-10 13:16:40 -0400

    Updated from repl.it

--- main.py
+++ main.py
@@ -1,5 +1,62 @@
-import tables, game, qbot, config
+import pygame
+from pygame.locals import *
+import storage
+
+
 class Game():
-  
-    def __init__(self):
-      self.qtable = {}
+	whitescore = 0
+	blackscore = 0
+
+	whitepieces = {
+		"p1":[0,0],
+		"p2":[0,0],
+		"p3":[0,0],
+		"p4":[0,0],
+		"p5":[0,0],
+		"p6":[0,0],
+		"p7":[0,0],
+		"p8":[0,0],
+		"b1":[0,0],
+		"b2":[0,0],
+		"k1":[0,0],
+		"k2":[0,0],
+		"r1":[0,0],
+		"r2":[0,0],
+		"K":[0,0],
+		"Q":[0,0]
+	}
+
+	blackpieces = {
+		"p1":[0,0],
+		"p2":[0,0],
+		"p3":[0,0],
+		"p4":[0,0],
+		"p5":[0,0],
+		"p6":[0,0],
+		"p7":[0,0],
+		"p8":[0,0],
+		"b1":[0,0],
+		"b2":[0,0],
+		"k1":[0,0],
+		"k2":[0,0],
+		"r1":[0,0],
+		"r2":[0,0],
+		"K":[0,0],
+		"Q":[0,0]
+	}
+
+	def whiteisalive(self, piece):
+		if self.whitepieces[piece] == None:
+			return False
+		else:
+			return True
+
+	def blackisalive(self, piece):
+		if self.blackpieces[piece] == None:
+			return False
+		else:
+			return True
+
+
+#pygame.init()
+
---
+++ storage.py
@@ -0,0 +1,87 @@
+import sqlite3
+import pickle
+
+db = sqlite3.connect("tables.db")
+c = db.cursor()
+
+def checktables():
+	try:
+		c.execute("SELECT * FROM QTables")
+	except:
+
+		CreateQTable = [] #{state: dict, qvalue: int}
+		c.execute("""CREATE TABLE QTables (
+			GameID int,
+			QTable BLOB
+		)""")
+		db.commit()
+
+	try:
+		c.execute("SELECT * FROM BotActions")
+	except:
+
+		# CreateBotActions = {
+		# 	p1: { actions:[], alive:True },
+		# 	p2: { actions:[], alive:True },
+		# 	p3: { actions:[], alive:True },
+		# 	p4: { actions:[], alive:True },
+		# 	p5: { actions:[], alive:True },
+		# 	p6: { actions:[], alive:True },
+		# 	p7: { actions:[], alive:True },
+		# 	p8: { actions:[], alive:True },
+		# 	k1: { actions:[], alive:True },
+		# 	k2: { actions:[], alive:True },
+		# 	b1: { actions:[], alive:True },
+		# 	b2: { actions:[], alive:True },
+		# 	r1: { actions:[], alive:True },
+		# 	r2: { actions:[], alive:True },
+		# 	K: { actions:[], alive:True },
+		# 	Q: { actions:[], alive:True },
+		# }
+		c.execute("""CREATE TABLE BotActions (
+			GameID int,
+			ActionTable BLOB
+		)""")
+		db.commit()
+
+def updateactiontable(table, gameid):
+	try:
+		c.execute("SELECT * FROM BotActions WHERE gameid="+gameid)
+	except sqlite3.Error as err:
+		raise sqlite3.Error(er)
+	else:
+		pickledtable = pickle.dumps(table)
+		c.execute("UPDATE BotActions SET ActionTable="+pickledtable+" WHERE gameid="+gameid)
+	finally:
+		db.commit()
+
+def getactiontable(gameid):
+	try:
+		c.execute("SELECT * FROM BotActions WHERE gameid="+gameid)
+	except sqlite3Error as err:
+		raise sqlite3.Error(er)
+	else:
+		table = c.fetchall()
+		actiontable = pickle.loads(table)
+		return actiontable
+
+def updateQtable(table, gameid):
+	try:
+		c.execute("SELECT * FROM QTables WHERE gameid="+gameid)
+	except sqlite3.Error as err:
+		raise sqlite3.Error(er)
+	else:
+		pickledtable = pickle.dumps(table)
+		c.execute("UPDATE QTables SET QTable")
+	finally:
+		db.commit()
+
+def getQtable(gameid):
+	try:
+		c.execute("SELECT * FROM QTables WHERE gameid="+gameid)
+	except sqlite.Error as err:
+		raise sqlite3.Error(er)
+	else:
+		table = c.fetchall()
+		qtable = pickle.loads(table)
+		return qtable
